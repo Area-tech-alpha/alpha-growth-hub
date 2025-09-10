18ffdd2e35bfc7cad43f57742d26620d413d2a97
Maxwell paulo <maxpaulo.mps@gmail.com>
2025-09-10 10:11:39 -0300

    feat: Enhance layout metadata with viewport settings
    - Added viewport settings to layout.tsx, including theme color adjustments for light and dark modes, improving user experience across different display preferences.
    - Updated the import statement to include Viewport type for better type safety in the metadata structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/providers/providers";
@@ -59,14 +59,17 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
-    { media: "(prefers-color-scheme: dark)", color: "#000000" },
-  ],
   robots: {
     index: true,
     follow: true,
   },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#facc15" },
+    { media: "(prefers-color-scheme: dark)", color: "#ca8a04" },
+  ],
 };
 
 export default function RootLayout({
