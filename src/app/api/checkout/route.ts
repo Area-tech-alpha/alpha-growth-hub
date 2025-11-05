8164eaa029f20bea2059f08af028c75df66de06e
Maxwell paulo <maxpaulo.mps@gmail.com>
2025-11-05 15:02:25 -0300

    Revert "console asaas response"
    This reverts commit acb9c44634e39073efc93f08cc202c8041549930.

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -135,8 +135,6 @@ export async function POST(request: Request) {
             }
         }
 
-        console.log("asaasResponse", asaasResponse)
-
         const payload = {
             success: true,
             checkoutUrl: asaasResponse.link,
